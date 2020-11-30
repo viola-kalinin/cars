16c007f5d4419eef86fbab31b74134c16e29e792
Viola Kalinin <viola.kalinin@gmail.com>
2020-11-30 11:21:07 -0800

    Adds cars.sh

---
+++ cars.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+#cars.sh
+#Viola Kalinin
+echo "Type 1 to Add a car"
+echo "Type 2 to List the cars in the inventory file"
+echo "Type 3 to Quit the program"
+echo "What would you like to do?" 
+read Option
+while :
+do 
+	case $Option in 
+		1)
+			string=""
+			echo "Enter year: "
+			read year
+			string+=$year
+			string+=":"
+			echo "Enter make: "
+                        read make
+			string+=$make
+                        string+=":"
+			echo "Enter model: "
+                        read model
+			string+=$model 
+			echo $string >> My_old_cars
+			echo $string 
+			echo "Car added!";; 
+		2)
+			sort My_old_cars;;
+		3)
+			echo "Goodbye!" 
+			break;;
+	esac
+	echo "What would you like to do?"
+	read Option
+done
+  
