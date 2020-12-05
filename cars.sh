c8dd95991a84485ddd7e98e9bcfd98b410904a14
Viola Kalinin <viola.kalinin@gmail.com>
2020-12-04 19:55:19 -0800

    Adds cars.sh

--- cars.sh
+++ cars.sh
@@ -5,24 +5,24 @@ echo "Type 1 to Add a car"
 echo "Type 2 to List the cars in the inventory file"
 echo "Type 3 to Quit the program"
 echo "What would you like to do?" 
-read Option
+read -r Option
 while :
 do 
 	case $Option in 
 		1)
 			string=""
 			echo "Enter year: "
-			read year
+			read -r year
 			string+=$year
 			string+=":"
 			echo "Enter make: "
-                        read make
+                        read -r make
 			string+=$make
                         string+=":"
 			echo "Enter model: "
-                        read model
+                        read -r model
 			string+=$model 
-			echo $string >> My_old_cars;; 
+			echo "$string" >> My_old_cars;; 
 		2)
 			sort My_old_cars;;
 		3)
@@ -30,6 +30,6 @@ do
 			break;;
 	esac
 	echo "What would you like to do?"
-	read Option
+	read -r Option
 done
   
